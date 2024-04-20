90ba910c0e94728c7359c6e1934cdfcb1baca04b
Judit-Camps <campsjudit99@gmail.com>
2024-04-20 20:43:24 +0200

    add base files for com folder - validate, errors, util #440

---
+++ staff/judit-camps/project/com/validate.ts
@@ -0,0 +1,35 @@
+import { ContentError, UnauthorizedError } from "./errors"
+import util from "./util"
+
+const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const PASSWORD_REGEX = /^(?=.*[0-9])(?=.*[A-Za-z])[A-Za-z0-9]+$/
+
+const validate = {
+    text(text, explain, checkEmptySpaces?) {
+        if (typeof text !== "string") throw new TypeError(`${explain}: ${text} is not a stirng`)
+        if (!text.trim().length) throw new ContentError(`${explain}: >${text}< is empty or blank`)
+
+        if (checkEmptySpaces) {
+            if (text.includes(" ")) throw new ContentError(`${explain}: ${text} has empty spaces`)
+        }
+    },
+
+    password(password, explain = "password") {
+        if (!PASSWORD_REGEX.test(password)) throw new ContentError(`${explain} is not valid`)
+    },
+
+    email(email, explain = "email") {
+        if (!EMAIL_REGEX.test(email)) throw new ContentError(`${explain} is not valid`)
+    }
+
+    // token(token, explain = "token") {
+    //     if (typeof token !== "string") throw new TypeError(`${explain} is not a string`)
+
+    //     const { exp } = util.extractJwtPayload(token)
+
+    //     if (exp * 1000 < Date.now()) throw new UnauthorizedError("session expired")
+    // }
+
+}
+
+export default validate
